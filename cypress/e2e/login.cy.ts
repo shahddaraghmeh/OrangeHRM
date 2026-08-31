d0820801a139b09a1a6b10d74716601abf203197
Shahd Daraghmeh <Shahddaraghmah9@gmail.com>
2026-08-31 16:08:52 +0300

    Add OrangeHRM login tests

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,64 @@
+
+describe("Login Page Tests", () => {
+
+  beforeEach(() => {
+    cy.log("1");
+    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+  });
+
+  const username = "Admin";
+  const password = "admin123";
+
+
+  it("TC01: Verify login with valid username and password", () => {
+
+    cy.get(".orangehrm-login-form").find('input[placeholder="Username"]').type(username);
+    cy.get(".orangehrm-login-form").find('input[placeholder="Password"]').type(password);
+    cy.get(".orangehrm-login-form").find("button").contains("Login").click();
+   
+    cy.url().should("eq", "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
+    cy.get(".oxd-topbar-header-breadcrumb").find("h6").should("have.text", "Dashboard");
+  });
+
+
+  it("TC02: Verify login with invalid username", () => {
+
+    cy.get(".orangehrm-login-form").find('input[placeholder="Username"]').type("Admin123");
+    cy.get(".orangehrm-login-form").find('input[placeholder="Password"]').type(password);
+    cy.get(".orangehrm-login-form").find("button").contains("Login").click();
+    cy.get(".orangehrm-login-error").contains("Invalid credentials").should("have.text", "Invalid credentials");
+  });
+
+
+  it("TC03: Verify login with invalid password", () => {
+
+    cy.get(".orangehrm-login-form").find('input[placeholder="Username"]').type(username);
+    cy.get(".orangehrm-login-form").find('input[placeholder="Password"]').type("123456");
+    cy.get(".orangehrm-login-form").find("button").contains("Login").click();
+    cy.get(".orangehrm-login-error").contains("Invalid credentials").should("have.text", "Invalid credentials");
+  });
+
+
+  it("TC04: Verify login without username", () => {
+
+    cy.get(".orangehrm-login-form").find('input[placeholder="Password"]').type(password);
+    cy.get(".orangehrm-login-form").find("button").contains("Login").click();
+    cy.get(".orangehrm-login-form").contains("Required").should("have.text", "Required");
+  });
+
+
+  it("TC05: Verify login without password", () => {
+
+    cy.get(".orangehrm-login-form").find('input[placeholder="Username"]').type(username);
+    cy.get(".orangehrm-login-form").find("button").contains("Login").click();
+    cy.get(".orangehrm-login-form").contains("Required").should("have.text", "Required");
+  });
+
+
+  it("TC06: Verify login with empty username and password", () => {
+    cy.get(".orangehrm-login-form").find("button").contains("Login").click();
+    cy.get(".orangehrm-login-form").contains("Required").should("have.text", "Required");
+  });
+
+});
+
