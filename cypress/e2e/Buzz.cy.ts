55fed975e17ab316dc420fc04c507d89bb95a8ac
Shahd Daraghmeh <Shahddaraghmah9@gmail.com>
2026-09-07 16:37:40 +0300

    Update Buzz tests

--- cypress/e2e/Buzz.cy.ts
+++ cypress/e2e/Buzz.cy.ts
@@ -5,20 +5,26 @@ describe("Buzz Page Tests", () => {
 
   beforeEach(() => {
     cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-    cy.get(".orangehrm-login-form").find('input[placeholder="Username"]').type(username);
-    cy.get(".orangehrm-login-form").find('input[placeholder="Password"]').type(password);
+    cy.get(".orangehrm-login-form").find('input[name="username"]').type(username);
+    cy.get(".orangehrm-login-form").find('input[name="password"]').type(password);
+    cy.intercept("GET", "/web/index.php/dashboard/index").as("DASHBOARDREQUEST");
     cy.get(".orangehrm-login-form").find("button").contains("Login").click();
-    cy.url().should(  "eq", "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
+    cy.wait("@DASHBOARDREQUEST").its("response.statusCode").should("eq", 200);;
+
   });
 
-  it("TC01: Verify user can create a post using fixture data", () => {
+  it("TC07: Verify user can create a post using fixture data", () => {
+
     cy.fixture("BuzzPost.json").then((data) => {
+      cy.intercept("GET", "**/web/index.php/api/v2/buzz/feed**").as("FEEDREQUEST");
+
       cy.get(".oxd-main-menu-item").contains("Buzz").click();
-      cy.url().should("eq", "https://opensource-demo.orangehrmlive.com/web/index.php/buzz/viewBuzz");
-      cy.get(".orangehrm-buzz-create-post") .find(".oxd-buzz-post-input").type(data.postText);
+      cy.wait("@FEEDREQUEST").its("response.statusCode").should("eq", 200);
+      cy.intercept("POST", "**/api/v2/buzz/posts").as("postRequest");
+      cy.get(".orangehrm-buzz-create-post").find(".oxd-buzz-post-input").type(data.postText);
       cy.get(".orangehrm-buzz-create-post").find("button").contains("Post").click();
-      cy.get(".orangehrm-buzz-post-body-text") .contains(data.postText).should("be.visible");
+      cy.wait("@postRequest").its("response.statusCode").should("eq", 200);
+      cy.contains('.orangehrm-buzz-post-body-text', data.postText).should('be.visible');
     });
   });
-
 });
